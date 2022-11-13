d9f24b6d2e6a52e1690f6853759c7b91b3e5ebf8
Jazz Loffredo <jzloffredo@gmail.com>
2022-11-13 14:58:05 -0600

    Add firebase hosting, basic app configuration from defaults.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFunctions(() => getFunctions())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'proj-archimedes',
+    appId: '1:225299118491:web:03539a91dc85eead84093e',
+    storageBucket: 'proj-archimedes.appspot.com',
+    apiKey: 'AIzaSyBA1c2lvTW8tU9heCjuRVgluXBicDG5jxM',
+    authDomain: 'proj-archimedes.firebaseapp.com',
+    messagingSenderId: '225299118491',
+    measurementId: 'G-PTLSREEXZ5',
+  },
   production: true
 };
